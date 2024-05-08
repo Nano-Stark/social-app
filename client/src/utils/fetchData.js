a5a8db5550822a1cba8b5b9ca74ef6273ad84f1c
Stark <inventorstark7@gmail.com>
2024-05-08 04:40:47 +0100

    fix: use correct import path

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,5 +1,5 @@
 // import axios from 'axios';
-import axiosInstance from "axiosInstance"
+import axiosInstance from "./axiosInstance"
 
 export const getDataAPI = async (url,token) => {
     const res = await axiosInstance.get(`/api/${url}`,{
