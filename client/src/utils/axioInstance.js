2b42e49ef89d7d25644565797cdfa3b807e199c4
Stark <inventorstark7@gmail.com>
2024-05-08 02:11:12 +0100

    feat: client

---
+++ client/src/utils/axioInstance.js
@@ -0,0 +1,9 @@
+// remove proxy from package.json -> not needed
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    withCredentials: true,
+})
+
+export default axiosInstance
